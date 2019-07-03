ad638797a97419370bb158c1e6281df1dffe88d5
Evan Peterson <evanpeterson17@gmail.com>
2019-07-03 09:31:16 -0600

    Add feature for counting number of documents in each index

--- src/dump.py
+++ src/dump.py
@@ -7,23 +7,51 @@ from tqdm import tqdm
 from client import client
 from settings import INDEXES
 
-# Configure argument parser
-parser = argparse.ArgumentParser(description='Make a local dump of the D3M MtL Database.')
-parser.add_argument("--out-dir", "-o", default="dump", help='The path to the folder the dump will be written to (include the folder name)')
-parser.add_argument("--batch-size", "-b", type=int, default=100, help='The number of records to read from the database at a time')
-args = parser.parse_args()
 
-# Read the indexes and dump them to args.out_dir
-for name_key in INDEXES:
-    index_name = INDEXES[name_key]
-    out_name = f"{args.out_dir}/{index_name}.json"
+def get_parser():
+    """Configures the argument parser for dumping or counting the database."""
+    parser = argparse.ArgumentParser(description='Make a local dump of the D3M MtL Database.')
+    parser.add_argument("--out-dir", "-o", default="dump", help='The path to the folder the dump will be written to (include the folder name)')
+    parser.add_argument("--batch-size", "-b", type=int, default=100, help='The number of records to read from the database at a time')
+    parser.add_argument("--count", "-c", action='store_true', help="If present, the dump will not take place; only the number of records in each index will be written out.")
+    return parser
 
-    s = Search(using=client, index=index_name)
-    num_docs_in_index = s.count()
-    print(f"Now writing index '{index_name}' ({num_docs_in_index} documents) to path '{out_name}'")
 
+def dump(out_dir, batch_size):
+    """Read the DB indexes and dump them to :out_dir:."""
+    for name_key in INDEXES:
+        index_name = INDEXES[name_key]
+        out_name = f"{out_dir}/{index_name}.json"
+
+        s = Search(using=client, index=index_name)
+        num_docs_in_index = s.count()
+        print(f"Now writing index '{index_name}' ({num_docs_in_index} documents) to path '{out_name}'")
+
+        with open(out_name, "w") as f:
+            for hit in tqdm(s.params(size=batch_size).scan(), total=num_docs_in_index):
+                # Paginate over this index
+                f.write(json.dumps(hit.to_dict()))
+                f.write("\n")
+
+
+def count(out_dir):
+    """Count the number of documents in each index of the DB."""
+    out_name = f"{out_dir}/index_counts.json"
+    counts = []
+    for name_key in INDEXES:
+        index_name = INDEXES[name_key]
+        num_docs_in_index = Search(using=client, index=index_name).count()
+        counts.append((index_name, num_docs_in_index))
     with open(out_name, "w") as f:
-        for hit in tqdm(s.params(size=args.batch_size).scan(), total=num_docs_in_index):
-            # Paginate over this index
-            f.write(json.dumps(hit.to_dict()))
-            f.write("\n")
+        for index, count in counts:
+            f.write(f"{index}\t{count}\n")
+    print(f"index counts written out to '{out_name}'")
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    if args.count:
+        count(args.out_dir)
+    else:
+        dump(args.out_dir, args.batch_size)
