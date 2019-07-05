ca603e22a4e7c3c0e4cbb523e29e533625c5cec0
Evan Peterson <evanpeterson17@gmail.com>
2019-07-05 15:43:21 -0600

    Add more to basic_stats analysis

--- src/analyses/basic_stats.py
+++ src/analyses/basic_stats.py
@@ -1,9 +1,14 @@
 from src.analyses.analysis import Analysis
 from src.utils import has_path
+from src.entities.pipeline import Pipeline
+from src.entities.primitive import Primitive
 
 
 class BasicStatsAnalysis(Analysis):
     def run(self, dataset: dict, verbose: bool):
+
+        # config
+        num_top_primitives = 10
 
         # How many runs are there?
         num_runs = len(dataset.keys())
@@ -15,8 +20,20 @@ class BasicStatsAnalysis(Analysis):
         metric_types_cnt: dict = {}
         # How many pipeline runs include a normalized value with their metrics?
         num_normalized_metric_values = 0
+        # Which primitives are most common?
+        primitives_cnt: dict = {}
+        # How many sub-pipelines are being used?
+        num_subpipelines = 0
 
         for run in dataset.values():
+
+            for step in run.pipeline.steps:
+                if isinstance(step, Primitive):
+                    if step.python_path not in primitives_cnt:
+                        primitives_cnt[step.python_path] = 0
+                    primitives_cnt[step.python_path] += 1
+                elif isinstance(step, Pipeline):
+                    num_subpipelines += 1
 
             num_scores_str = str(len(run.scores))
             if num_scores_str not in metrics_cnts:
@@ -39,6 +56,11 @@ class BasicStatsAnalysis(Analysis):
                 phase_cnts[run.run_phase] = 0
             phase_cnts[run.run_phase] += 1
 
+        primitives_cnt_tuples = primitives_cnt.items()
+        primitives_cnt_tuples = sorted(
+            primitives_cnt_tuples, key=lambda toop: toop[1], reverse=True
+        )
+
         # Report
         print("\n*********************")
         print("****** RESULTS ******")
@@ -50,3 +72,7 @@ class BasicStatsAnalysis(Analysis):
         print(
             f"There are {num_normalized_metric_values} normalized metric values found among the pipelines"
         )
+        print(f"The {num_top_primitives} most commonly used primitives are:")
+        for prim_cnt in primitives_cnt_tuples[:num_top_primitives]:
+            print(f"\t{prim_cnt[0]}\t{prim_cnt[1]}")
+        print(f"There are {num_subpipelines} sub-pipelines found among the runs")
