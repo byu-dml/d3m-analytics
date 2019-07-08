0d4800a3d33f0ae958019a63a2b7fd4f8c613dba
Evan Peterson <evanpeterson17@gmail.com>
2019-07-08 14:38:31 -0600

    Clean up data reference class

--- src/entities/data_reference.py
+++ src/entities/data_reference.py
@@ -11,14 +11,10 @@ class DataReference:
         """
         Example of reference_string: 'steps.0.produce' or 'inputs.1'
         """
-        try:
-            parts = reference_string.split(".")
-        except AttributeError:
-            print(reference_string)
-            raise AttributeError()
+        parts = reference_string.split(".")
         self.type: str = parts[0]
         self.index: int = int(parts[1])
         self.method_name: Optional[str] = parts[2] if len(parts) > 2 else None
         if len(parts) > 3:
-            raise Exception(f"unknown data reference string '{reference_string}'")
+            raise ValueError(f"unknown data reference string '{reference_string}'")
 
