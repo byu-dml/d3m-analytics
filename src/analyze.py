ebdc417e612cc6bf91720f262d51d947410cae1e
Evan Peterson <evanpeterson17@gmail.com>
2019-08-21 12:51:36 -0600

    Factor out method for reuse

--- src/analyze.py
+++ src/analyze.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from typing import Type, Mapping
+from typing import Type, Mapping, Dict
 import pickle
 
 from src.misc.settings import DefaultDir, DefaultFile
@@ -57,13 +57,17 @@ def get_parser() -> ArgumentParser:
     return parser
 
 
+def load_entity_maps_pkl(pkl_dir: str = DefaultDir.EXTRACTION.value) -> Dict[str, dict]:
+    read_path = f"{pkl_dir}/{DefaultFile.EXTRACTION_PKL.value}"
+    print(f"Now loading pickled entity maps from '{read_path}'...")
+    with open(read_path, "rb") as f:
+        return pickle.load(f)
+
+
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    read_path = f"{args.pkl_dir}/{DefaultFile.EXTRACTION_PKL.value}"
-    print(f"Now loading pickled pipeline_runs from '{read_path}'...")
-    with open(read_path, "rb") as f:
-        entity_maps: dict = pickle.load(f)
+    entity_maps = load_entity_maps_pkl(args.pkl_dir)
     analysis_class: Type[Analysis] = analysis_map[args.analysis]
     analysis = analysis_class()
     print(f"Now running {args.analysis} analysis...")
