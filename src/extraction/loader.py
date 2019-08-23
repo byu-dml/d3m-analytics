631eedfe3a4485caf474c9a9e7f6e48e2da817da
Evan Peterson <evanpeterson17@gmail.com>
2019-08-23 14:42:26 -0600

    Make index loader util be class agnostic

--- src/extraction/loader.py
+++ src/extraction/loader.py
@@ -1,5 +1,5 @@
 import json
-from typing import Type, Dict
+from typing import Type, Dict, Callable, Hashable, Any
 
 from tqdm import tqdm
 
@@ -8,28 +8,26 @@ from src.misc.settings import Index
 from src.entities.entity import EntityWithId
 
 
-def _load_index(
-    dump_path: str,
-    index: Index,
-    entity_sub_class: Type[EntityWithId],
-    should_enforce_id: bool,
-):
+def _load_index(dump_path: str, index: Index, processor: Callable, *args, **kwargs):
     """
     Loads a single document from a dumped index,
-    returning one document at a time instantiated as an `entity_sub_class`.
+    returning one document at a time processed by `processor`.
     Uses a generator so the entire index doesn't need to be loaded
     into memory at once.
     
     Parameters
     ----------
-    dump_path : str
+    dump_path
         The filepath to the dump directory (include directory name)
-    index : Index
+    index
         The name of the database index to read from
-    entity_sub_class: Type[EntityWithId]
-        A reference to a class that inherits from EntityWithId.
-        Each document read from the index is passed through its
-        constructor.
+    processor
+        Each document read from the index is passed through this
+        callable.
+    *args
+        Any remaining positional args passed into `processor`.
+    **kwargs
+        Any remaining key-word args passed into `processor`.
     """
     file_path = f"{dump_path}/{index.value}.json"
     num_docs = file_len(file_path)
@@ -39,9 +37,31 @@ def _load_index(
                 line = f.readline()
                 if not line:
                     break
-                entity_dict = json.loads(line)
-                yield entity_sub_class(entity_dict, should_enforce_id)
+                data = json.loads(line)
+                yield processor(data, *args, **kwargs)
                 progress_bar.update(1)
+
+
+def load_index_map(
+    dump_path: str,
+    index: Index,
+    processor: Callable,
+    key_getter: Callable,
+    *args,
+    **kwargs,
+):
+    """
+    Loads an entire DB index as a map of processed documents i.e. to
+    document keys identified by `key_getter`. E.g. a key-value pair
+    in the returned map will look like this:
+    `key_getter(processor(document)): processor(document)`.
+    """
+    index_map: Dict[Hashable, Any] = {}
+    print(f"Now loading `{index.value}`...")
+    for processed_document in _load_index(dump_path, index, processor, *args, **kwargs):
+        index_map[key_getter(processed_document)] = processed_document
+
+    return index_map
 
 
 def load_entity_map(
@@ -50,9 +70,11 @@ def load_entity_map(
     entity_sub_class: Type[EntityWithId],
     should_enforce_id: bool,
 ):
-    entity_map: Dict[str, EntityWithId] = {}
-    print(f"Now loading `{index.value}`...")
-    for entity in _load_index(dump_path, index, entity_sub_class, should_enforce_id):
-        entity_map[entity.get_id()] = entity
-
-    return entity_map
+    """
+    Convenience loader for loading objects inheriting
+    from `EntityWithId`.
+    """
+    key_getter = lambda entity: entity.get_id()
+    return load_index_map(
+        dump_path, index, entity_sub_class, key_getter, should_enforce_id
+    )
