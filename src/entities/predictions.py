39ac1ba057f018db9bf10c19d1efd69493aef2a9
Evan Peterson <evanpeterson17@gmail.com>
2019-08-30 12:59:32 -0600

    Try to always make predictions numbers, to make comparisons less error prone

--- src/entities/predictions.py
+++ src/entities/predictions.py
@@ -1,11 +1,23 @@
 from typing import Tuple
 
 import pandas as pd
+import numpy as np
 
 
 class Predictions:
     def __init__(self, indices: list, values: list):
-        self.data = pd.DataFrame({"indices": indices, "values": values})
+        try:
+            # Convert to a number when possible, since they're
+            # more comparable e.g. 1 == 1.0, etc.
+            values_series = pd.Series(values, dtype=np.float)
+        except ValueError:
+            # Fall back to a string
+            values_series = pd.Series(values, dtype=np.str)
+
+        self.data = pd.DataFrame(
+            # Indices should always be parseable to a float
+            {"indices": pd.Series(indices, dtype=np.float), "values": values_series}
+        )
 
     @staticmethod
     def find_common(
