75f728aca284c4623178c1c4d6cbe8316666e01c
Evan Peterson <evanpeterson17@gmail.com>
2019-09-05 10:45:04 -0600

    Look for pipeline runs using exlines and track exline tantamount match sources

--- src/scripts/find_exlines_in_db.py
+++ src/scripts/find_exlines_in_db.py
@@ -1,49 +1,75 @@
 """
 Sees how many of the exline pipelines are found in the DB
-already. Saves to `SAVE_PATH` the digests of pipelines in
+already. Saves to `MATCHES_SAVE_PATH` the digests of pipelines in
 the DB that are tantamount to the Exline pipelines.
 """
 
 from collections import defaultdict
 from pprint import pprint
 import json
-from typing import Dict
+from typing import Dict, Optional, List
 
 from src.analyze import load_entity_maps_pkl
 from src.entities.pipeline import Pipeline
+from src.entities.pipeline_run import PipelineRun
 
-SAVE_PATH = "dump/exline-ids-to-digests-of-db-equivalents.json"
+MATCHES_SAVE_PATH = "dump/exline-ids-to-digests-of-db-equivalents.json"
+RUN_MATCHES_SAVE_PATH = "dump/pipeline-run-digests-of-exlines-or-matches.json"
 
-pipelines: Dict[str, Pipeline] = load_entity_maps_pkl()["pipelines"]
-exlines: Dict[str, Pipeline] = Pipeline.from_json_glob("dump/exlines/*.json", True)
+entity_maps = load_entity_maps_pkl()
+pipelines: Dict[str, Pipeline] = entity_maps["pipelines"]
+runs: Dict[str, PipelineRun] = entity_maps["pipeline_runs"]
+exlines: Dict[str, Pipeline] = Pipeline.from_json_glob("dump/exlines/*.json")
+
+# What is the distribution of source name across the pipelines
+# isomorphic to the exlines?
+source_name_cnt: Dict[Optional[str], int] = defaultdict(int)
 
 tantamount_matches: defaultdict = defaultdict(list)
 for exline in exlines.values():
     for pipeline in pipelines.values():
         if exline.is_tantamount_to(pipeline):
+            tantamount_matches[exline.id].append(pipeline.digest)
+            source_name_cnt[pipeline.source_name] += 1
             if len(tantamount_matches[exline.id]) == 0:
                 print(f"\nfirst match found for exline {exline.id}.")
                 print("exline steps:")
                 exline.print_steps()
                 print("matching pipeline steps:")
                 pipeline.print_steps()
-            tantamount_matches[exline.id].append(pipeline.digest)
+
 
 # Search for pipelines in the DB that have the same digests as
 # the exlines. If the digests are the same they should be exactly
 # equivalent.
-exact_matches: defaultdict = defaultdict(list)
+print("Now looking for exact matches by digest...")
 for exline in exlines.values():
     if exline.digest in pipelines:
-        exact_matches[exline.id].append(pipeline.digest)
+        print(f"found exact match for exline with id={exline.id}")
 
 print("tantamount_matches (pipelines in DB tantamount to the exlines)")
 pprint(tantamount_matches)
 
-print("exact_matches (pipelines in DB with same digest as the exlines)")
-pprint(exact_matches)
+print(
+    f"The distribution of source names among tantamount matches is: {source_name_cnt}"
+)
 
-with open(SAVE_PATH, "w") as f:
+with open(MATCHES_SAVE_PATH, "w") as f:
     json.dump(tantamount_matches, f, indent=4)
 
-print(f"tantamount_matches written to '{SAVE_PATH}'")
+print(f"tantamount_matches written to '{MATCHES_SAVE_PATH}'")
+
+# Unpack all the match digests and exline digests into a single list
+match_digests = [match for matches in tantamount_matches.items() for match in matches]
+match_digests += exlines.keys()
+
+runs_using_exlines_or_matches: List[str] = []
+# Find any pipeline runs using a match or exline pipeline
+for digest in match_digests:
+    for run in runs.values():
+        if digest == run.pipeline.get_id():
+            runs_using_exlines_or_matches.append(run.get_id())
+
+with open(RUN_MATCHES_SAVE_PATH, "w") as f:
+    json.dump(runs_using_exlines_or_matches, f)
+print(f"pipeline run matches written to {RUN_MATCHES_SAVE_PATH}")
