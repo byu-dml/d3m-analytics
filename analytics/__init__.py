2df05683e7291715916438784163c63f17706922
epeters3 <evanpeterson17@gmail.com>
2020-07-10 11:57:39 -0600

    Update package doc strings

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -1,5 +1,5 @@
 """
-A suite of tools for interacting with the D3M MtL DB.
-Also a series of analyses that can be conducted on the DB.
-See the docs here: https://github.com/byu-dml/d3m-mtl-db-reader.
+A suite of tools for interacting with the D3M MtL DB,
+including data warehousing and analysis.
+See the docs here: https://github.com/byu-dml/d3m-analytics.
 """
